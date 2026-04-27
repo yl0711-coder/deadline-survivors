e32b13d896d037803c5ea273bb72ced09722f0cb
Yang Lei <yanglei711@gmail.com>
2026-04-27 21:07:32 +0800

    Move upgrade application into progression module

--- src/deadline_survivors/game.py
+++ src/deadline_survivors/game.py
@@ -1,4 +1,5 @@
 """Public game entrypoint and compatibility exports."""
 
+from .constants import ACCENT, GREEN  # noqa: F401
 from .content import ENEMY_TYPES, OUTAGE_BOSS, UPGRADES  # noqa: F401
 from .modules.runtime import *  # noqa: F403
